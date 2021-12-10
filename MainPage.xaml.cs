d072dd83035d1f1d3c3bac3aec1596211156d3d5
Codedbeard <s_croxford@hotmail.com>
2021-12-10 19:13:39 +0000

    repo

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using CommunityToolkit.Maui.Alerts.Snackbar;
+
+namespace notoast
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void OnCounterClicked(object sender, EventArgs e)
+		{
+			await this.DisplaySnackbar("TEST");
+			count++;
+			CounterLabel.Text = $"Current count: {count}";
+
+			SemanticScreenReader.Announce(CounterLabel.Text);
+		}
+	}
+}
